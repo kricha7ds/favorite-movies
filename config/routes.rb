f9ff8b68606c55a968a90228b99137209d167006
Katherine Richards <kricha7ds@gmail.com>
2023-04-11 14:44:09 -0600

    add resoource for tmdb movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :movie_lists
+  resources :movie_lists, :movies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root "movies#index"
   root "movie_lists#index"
