aff900a3e9c03cea8fff45b30d20946085b7f64d
Katherine Richards <kricha7ds@gmail.com>
2023-04-11 13:41:01 -0600

    config MovieList index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :movie_lists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "movies#index"
+  # root "movies#index"
+  root "movie_lists#index"
 end
